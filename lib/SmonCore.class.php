bac9bd0aa79a08d636041f5c1328b67fd9efe6c9
masted <masted311@gmail.com>
2014-04-13 14:34:28 +0400

    Commit was made from server localhost by ci/push

--- lib/SmonCore.class.php
+++ lib/SmonCore.class.php
@@ -2,19 +2,6 @@
 
 class SmonCore {
 
-  function getEpromoListOld() {
-    $sample = [
-      'host' => '176.9.151.229',
-      'pass' => 'jsdh8132u9fr'
-    ];
-    $r = [];
-    foreach ([22107, 22109] as $port) {
-      $sample['port'] = $port;
-      $r[] = $sample;
-    }
-    return $r;
-  }
-
   function getServerByPort($port) {
     return Arr::first(array_filter($this->getServers(), function ($v) use ($port) {
       return (in_array($v['port'], [$port]));
@@ -31,42 +18,18 @@ class SmonCore {
     return isset($server['name']) ? $server['name'] : "{$server['user']}@{$server['host']}:{$server['port']}";
   }
 
+  protected function cmdCache($server, $cmd) {
+    $hash = implode('', $server).$cmd;
+    $file = __DIR__.'/.cmdCache.php';
+    //FileVar::formatVar()
+  }
+
+
   function cmd(array $server, $cmd) {
+    if (($r = $this->cmdCache($server, $cmd))) {
+      return $this->cache($server, $cmd);
+    }
     return trim(`ssh {$server['user']}@{$server['host']} -p {$server['port']} $cmd`);
-  }
-
-  function getEpromoListDocean() {
-    return [
-      [
-        'host' => 'localhost',
-        'pass' => 'qwdqwd',
-        'name' => 'epromo: sman'
-      ],
-    ];
-  }
-
-  function getPersonalListDocean() {
-    return [
-      [
-        'user' => 'root',
-        'host' => '82.196.14.175',
-        'pass' => 'fefg8fy23q',
-        'name' => 'personal: dnsMaster'
-      ],
-      [
-        'host' => '37.139.26.212',
-        'pass' => '23gy8f',
-        'name' => 'personal: projects1'
-      ],
-      /*
-      [
-        'user' => 'root',
-        'host' => '198.211.120.127',
-        'pass' => 'slrimeiivsxu',
-        'name' => 'personal: sman'
-      ]
-      */
-    ];
   }
 
   function getServers() {
@@ -74,10 +37,7 @@ class SmonCore {
       if (!isset($v['user'])) $v['user'] = 'user';
       if (!isset($v['port'])) $v['port'] = 22;
       return $v;
-    }, array_merge( //
-      $this->getEpromoListOld(), //$this->getEpromoListDocean(), //
-      $this->getPersonalListDocean() //
-    ));
+    }, require dirname(__DIR__).'/.servers.php');
   }
 
 }
