3382d8ea7b7667f065724cbfac0800a522f619a5
masted <masted311@gmail.com>
2015-11-27 14:15:50 +0300

    init commit on i-46 branch

--- web/site/lib/CtrlSmonDefault.class.php
+++ web/site/lib/CtrlSmonDefault.class.php
@@ -3,15 +3,8 @@
 class CtrlSmonDefault extends CtrlDefault {
 
   function action_default() {
-    $this->d['status'] = `ci status`;
-    $this->d['lastCaptureTime'] = file_get_contents(SMON_PATH.'/.capture');
-    $this->d['captures'] = array_map(function($file) {
-      return [
-        'title' => str_replace('.june.majexa.ru_sm.png', '', basename($file)),
-        'link' => 'http://'.str_replace('_sm.png', '', basename($file)).'/cpanel',
-        'img' => $file
-      ];
-    }, glob('captures/*_sm*'));
+    $servers = require PROJECT_PATH.'/.servers.php';
+    die2($servers);
   }
 
 }
---
+++ web/site/lib/CtrlSmonDefaultOld.class.php
@@ -0,0 +1,17 @@
+<?php
+
+class CtrlSmonDefaultOld extends CtrlDefault {
+
+  function action_default() {
+    $this->d['status'] = `ci status`;
+    $this->d['lastCaptureTime'] = file_get_contents(SMON_PATH.'/.capture');
+    $this->d['captures'] = array_map(function($file) {
+      return [
+        'title' => str_replace('.june.majexa.ru_sm.png', '', basename($file)),
+        'link' => 'http://'.str_replace('_sm.png', '', basename($file)).'/cpanel',
+        'img' => $file
+      ];
+    }, glob('captures/*_sm*'));
+  }
+
+}
