f07fd787e3781d45cd15f1a406731248f1a92f42
masted <masted311@gmail.com>
2015-12-08 13:50:23 +0300

    ...

--- web/index.php
+++ web/index.php
@@ -7,39 +7,74 @@ foreach ($statuses as $k => $v) {
 
 ?>
 <style>
-  table td {
+  body {
+    font-family: arial, helvetica, sans-serif;
+  }
+  .server img {
+    display: block;
+    margin-top: 30px;
+    margin-bottom: 10px;
+  }
+  .server {
+    font-size: 14px !important;
+    text-align: center;
+  }
+  table.main > tbody > tr > td {
     vertical-align: top;
     padding-right: 30px;
+    font-size: 11px;
   }
-  table li {
-    list-style: none;
+  table b {
+    color: #808080;
+    font-weight: bold;
+    text-decoration: underline;
+    display: block;
+    margin-bottom: 10px;
+    font-size: 14px;
+  }
+  table table td {
+    font-size: 14px;
+    padding-right: 15px;
+  }
+  table table {
+    width: 100%;
+  }
+  table table .name {
+    width: 100%;
+  }
+  .time {
+    font-size: 11px;
+    color: #808080;
+    padding-right: 0;
   }
 </style>
-<table>
+
+<table width="100%" class="main">
   <tr>
-    <td>
-      <img src="/m/img/server.png"><br>
-      staging
+    <td class="server">
+      <img src="/m/img/server.png">
+      tester
     </td>
     <td>
       <b>branches</b>
-      <ul>
-      <? foreach ($statuses as $v) { ?>
-        <li>
-          <a href="#" onclick="document.getElementById('<?= $v['name'] ?>').display='block';">
-            <img src="/m/img/<?= $v['success'] ? 'passed' : 'failed' ?>.png">
-            &nbsp;<?= date('d.m.Y H:i', $v['time']) ?>
-            <?= $v['name'] ?>
-            </li>
-          </a>
-      <? } ?>
-      </ul>
+      <table style="width:180px">
+        <? foreach ($statuses as $v) { ?>
+          <tr>
+            <td width="1"><img src="/m/img/<?= $v['success'] ? 'passed' : 'failed' ?>.png"></td>
+            <td class="name"><?= $v['name'] ?></td>
+            <td class="time"><?= date('H:i', $v['time']) ?></td>
+          </tr>
+        <? } ?>
+      </table>
     </td>
-    <td>
+    <td width="100%">
       <? foreach ($statuses as $k => $v) { ?>
-        <pre id="<?= $v['name'] ?>" styl e="display:none">
+        <? if (!empty($v['errors'])) { ?>
+          <b>errors on <?= $v['name'] ?></b>
+          <pre id="<?= $v['name'] ?>">
 <?= $v['errors'] ?>
-        </pre>
+          </pre>
+        <? } ?>
       <? } ?>
     </td>
   </tr>
